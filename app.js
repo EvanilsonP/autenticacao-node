559031f2098bd4d252de138d058044a99ebeb543
EvanilsonP <pevanilson99@gmail.com>
2023-03-20 15:14:16 -0300

    user login and token

--- app.js
+++ app.js
@@ -16,6 +16,39 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: "Welcome to this API." });
 });
 
+// private route
+app.get('/user/:id', checkToken, async(req, res) => {
+    const id = req.params.id;
+    // check if user exists
+    const user = await User.findById(id, '-password'); // -passwords deletes the user password in the body
+
+    if(!user) {
+        return res.status(404).json({ msg: "user not found"})
+    }
+
+    res.status(200).json({user});
+
+});
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token) {
+        return res.status(401).json({ msg: "access denied"})
+    }
+
+    try {
+        const secret = process.env.SECRET;
+        jwt.verify(token, secret);
+        next();
+    } 
+    
+    catch (err) {
+        res.status(404).json({msg: "invalid token"})
+    }
+}
+
 // register user
 app.post('/auth/register', async(req, res) => {
     const { name, email, password, confirmpassword } = req.body;
@@ -47,7 +80,7 @@ app.post('/auth/register', async(req, res) => {
 
     // create user
     const user = new User({
-        name, email, password
+        name, email, password: passwordHash
     });
 
     try {
@@ -57,6 +90,44 @@ app.post('/auth/register', async(req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).json({ msg: "Something went wrong. Try again later."})
+    }
+});
+
+// login user
+app.post('/auth/login', async(req, res) => {
+    const { email, password } = req.body;
+    // validation
+    if(!email) {
+        return res.status(422).json({ msg: 'email is required' });
+    }
+    if(!password) {
+        return res.status(422).json({ msg: 'password is required' });
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email });
+    if(!user) {
+        return res.status(404).json({ msg: 'User not found' });
+    }
+
+    // check if password match
+    const checkPassoword = await bcrypt.compare(password, user.password);
+    if(!checkPassoword) {
+        return res.status(422).json({ msg: 'invalid password' });
+    }
+
+    try {
+        const secret = process.env.SECRET;
+        const token = jwt.sign({
+            id: user._id,
+        }, secret)
+
+        res.status(200).json({msg: "authentication performed successfully", token})
+    } 
+
+    catch (err) {
+        console.log(err);
+        res.status(500).json({msg: "Somethint went wrong."});
     }
 });
 
