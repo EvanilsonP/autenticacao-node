7152027361bdb14b2b70d4879daf762a06cee1d0
EvanilsonP <pevanilson99@gmail.com>
2023-03-20 14:00:05 -0300

    handling post request

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const app = express();
 
+// config JSON response
+app.use(express.json());
+
+// open route - public route
 app.get('/', (req, res) => {
-    res.status(200).json({msg: "Welcome to this API."});
+    res.status(200).json({ msg: "Welcome to this API." });
 });
 
-app.listen(3000);
+// register user
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password, confirmpassword } = req.body;
+    // validations
+    if(!name) {
+        return res.status(422).json({ msg: 'Name is required' });
+    }
+});
+
+// credencials
+const dbuser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbPass}@cluster0.t2ptu42.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+    console.log('Connected.');
+}).catch((err) => console.log(err));
